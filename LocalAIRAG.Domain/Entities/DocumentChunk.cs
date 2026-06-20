ea91c365d2262b5b2cfb8638dc80ef2625dc6e69
Ogün EKİZ <ogunekiz@hotmail.com>
2026-06-20 20:29:01 +0300

    Add project files.

---
+++ LocalAIRAG.Domain/Entities/DocumentChunk.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalAIRAG.Domain.Entities
+{
+	public class DocumentChunk
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+		public string DocumentId { get; set; } = string.Empty;
+		public string Text { get; set; } = string.Empty;
+		public List<float> Embedding { get; set; } = new();
+	}
+}
